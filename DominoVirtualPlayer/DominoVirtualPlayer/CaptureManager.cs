5ec3355310fe945a843919c3e7a01854e2cc57cd
Alexei <kingslinskij@yandex.ua>
2016-07-30 16:17:37 +0300

    Made output from default camera

---
+++ DominoVirtualPlayer/DominoVirtualPlayer/CaptureManager.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+using Windows.System.Display;
+using Windows.UI.Xaml.Controls;
+// ReSharper disable ArrangeThisQualifier
+
+namespace DominoVirtualPlayer
+{
+    class CaptureManager
+    {
+        private readonly CaptureElement _captureElement;
+        // Prevent the screen from sleeping while the camera is running
+        private readonly DisplayRequest _displayRequest = new DisplayRequest();
+
+        // MediaCapture and its state variables
+        private MediaCapture _mediaCapture;
+        private bool _isInitialized;
+        private bool _isPreviewing;
+
+        public CaptureManager(CaptureElement input)
+        {
+            this._captureElement = input;
+        }
+
+        private async void MediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            Debug.WriteLine("MediaCapture_Failed: (0x{0:X}) {1}", errorEventArgs.Code, errorEventArgs.Message);
+
+            await CleanupCameraAsync();
+        }
+
+        /// <summary>
+        /// Initializes the MediaCapture, registers events, gets camera device information for mirroring and rotating, starts preview and unlocks the UI
+        /// </summary>
+        /// <returns></returns>
+        public async Task InitializeCameraAsync()
+        {
+            Debug.WriteLine("InitializeCameraAsync");
+
+            if (_mediaCapture == null)
+            {
+                // Attempt to get the back camera if one is available, but use any camera device if not
+                var cameraDevice = await FindDefaultCamera();
+
+                if (cameraDevice == null)
+                {
+                    Debug.WriteLine("No camera device found!");
+                    return;
+                }
+
+                // Create MediaCapture and its settings
+                _mediaCapture = new MediaCapture();
+
+                _mediaCapture.Failed += MediaCapture_Failed;
+
+                var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+
+                // Initialize MediaCapture
+                try
+                {
+                    await _mediaCapture.InitializeAsync(settings);
+                    _isInitialized = true;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Debug.WriteLine("The app was denied access to the camera");
+                }
+
+                // If initialization succeeded, start the preview
+                if (_isInitialized)
+                {
+                    await StartPreviewAsync();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Starts the preview and adjusts it for for rotation and mirroring after making a request to keep the screen on
+        /// </summary>
+        /// <returns></returns>
+        private async Task StartPreviewAsync()
+        {
+            // Prevent the device from sleeping while the preview is running
+            _displayRequest.RequestActive();
+
+            // Set the preview source in the UI and mirror it if necessary
+            this._captureElement.Source = _mediaCapture;
+
+            // Start the preview
+            await _mediaCapture.StartPreviewAsync();
+            _isPreviewing = true;
+        }
+
+        /// <summary>
+        /// Stops the preview and deactivates a display request, to allow the screen to go into power saving modes
+        /// </summary>
+        /// <returns></returns>
+        private async Task StopPreviewAsync()
+        {
+            // Stop the preview
+            _isPreviewing = false;
+            await _mediaCapture.StopPreviewAsync();
+
+            // Cleanup the UI
+            _captureElement.Source = null;
+
+            // Allow the device screen to sleep now that the preview is stopped
+            _displayRequest.RequestRelease();
+        }
+
+        /// <summary>
+        /// Cleans up the camera resources (after stopping any video recording and/or preview if necessary) and unregisters from MediaCapture events
+        /// </summary>
+        /// <returns></returns>
+        public async Task CleanupCameraAsync()
+        {
+            Debug.WriteLine("CleanupCameraAsync");
+
+            if (_isInitialized)
+            {
+                if (_isPreviewing)
+                {
+                    // The call to stop the preview is included here for completeness, but can be
+                    // safely removed if a call to MediaCapture.Dispose() is being made later,
+                    // as the preview will be automatically stopped at that point
+                    await StopPreviewAsync();
+                }
+
+                _isInitialized = false;
+            }
+
+            if (_mediaCapture != null)
+            {
+                _mediaCapture.Failed -= MediaCapture_Failed;
+                _mediaCapture.Dispose();
+                _mediaCapture = null;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to find and return a device mounted on the panel specified, and on failure to find one it will return the first device listed
+        /// </summary>
+        /// <param name="desiredPanel">The desired panel on which the returned device should be mounted, if available</param>
+        /// <returns></returns>
+        private static async Task<DeviceInformation> FindDefaultCamera()
+        {
+            // Get available devices for capturing pictures
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            return allVideoDevices.FirstOrDefault();
+        }
+
+    }
+}
--- DominoVirtualPlayer/DominoVirtualPlayer/MainPage.xaml.cs
+++ DominoVirtualPlayer/DominoVirtualPlayer/MainPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Windows.ApplicationModel;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+// ReSharper disable ArrangeThisQualifier
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +10,51 @@ namespace DominoVirtualPlayer
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
+        private readonly CaptureManager _captureManager;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            this._captureManager = new CaptureManager(this.capturElement);
+
+            Application.Current.Suspending += Application_Suspending;
+            Application.Current.Resuming += Application_Resuming;
+        }
+
+        private async void Application_Suspending(object sender, SuspendingEventArgs e)
+        {
+            // Handle global application events only if this page is active
+            if (Frame.CurrentSourcePageType == typeof(MainPage))
+            {
+                var deferral = e.SuspendingOperation.GetDeferral();
+
+                await this._captureManager.CleanupCameraAsync();
+
+                deferral.Complete();
+            }
+        }
+
+        private async void Application_Resuming(object sender, object o)
+        {
+            // Handle global application events only if this page is active
+            if (Frame.CurrentSourcePageType == typeof(MainPage))
+            {
+                await this._captureManager.InitializeCameraAsync();
+            }
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await this._captureManager.InitializeCameraAsync();
+        }
+
+        protected override async void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            // Handling of this event is included for completenes, as it will only fire when navigating between pages and this sample only includes one page
+            await this._captureManager.CleanupCameraAsync();
         }
     }
 }
